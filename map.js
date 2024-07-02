d444e220876cb17e882a61acd5865d2394137167
h69005am <aadityaa.mehrotra@student.manchester.ac.uk>
2024-07-02 13:17:44 +0100

    optimised code

--- map.js
+++ map.js
@@ -1,65 +1,87 @@
-var map = L.map('map').setView([45.519292, 11.338594], 8);
-var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+// Initialize the map
+const map = L.map('map').setView([45.519292, 11.338594], 8);
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     minZoom: 3,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-document.getElementById('00').addEventListener('click', function() {
-    document.getElementById('text').innerHTML = '<h1 id="heading">Climate Mapping</h1><a href="#" id="link-1"><h2 id="sub-heading-1">Intro</h2></a><p id="para-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Libero enim sed faucibus turpis in eu mi. Neque laoreet suspendisse interdum consectetur libero id. Mattis molestie a iaculis at erat pellentesque adipiscing. Diam maecenas sed enim ut sem. Commodo odio aenean sed adipiscing diam donec adipiscing. Amet tellus cras adipiscing enim eu turpis egestas. Nec dui nunc mattis enim ut tellus. Magna fermentum iaculis eu non diam phasellus vestibulum. Ac orci phasellus egestas tellus rutrum tellus pellentesque eu tincidunt. Enim nunc faucibus a pellentesque sit amet. Faucibus in ornare quam viverra orci sagittis eu. Aliquet risus feugiat in ante metus dictum at tempor commodo.</p><a href="#" id="link-2"><h2 id="sub-heading-2">Mid</h2></a><p id="para-2">Aliquam sem et tortor consequat id porta nibh venenatis. Mauris cursus mattis molestie a iaculis at erat pellentesque. Non enim praesent elementum facilisis leo. Leo vel orci porta non pulvinar neque. Netus et malesuada fames ac turpis egestas integer eget. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper. Vulputate dignissim suspendisse in est. Nec ullamcorper sit amet risus. Condimentum id venenatis a condimentum vitae sapien. Neque aliquam vestibulum morbi blandit cursus. Ut tellus elementum sagittis vitae et leo. Pulvinar sapien et ligula ullamcorper malesuada. Mauris pellentesque pulvinar pellentesque habitant.</p><a href="#" id="link-3"><h2 id="sub-heading-3">End</h2></a><p id="para-3">Lobortis elementum nibh tellus molestie nunc non blandit. Arcu dui vivamus arcu felis. A diam sollicitudin tempor id. Dignissim suspendisse in est ante in. Eu tincidunt tortor aliquam nulla facilisi cras. Elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl. Consequat ac felis donec et odio. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus. Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque. Fermentum odio eu feugiat pretium nibh ipsum consequat nisl. Platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper.</p>';
-    map.flyTo([45.519292, 11.338594], 8);
-    document.getElementById('progress-bar').style.width = '0%';
-});
+// Initialize variables
+let circleExists = false;
+let circle, circlee;
+let cannonsSet = false;
+let circleLayers = [];
+let videoExists = false;
 
-var circle;
-var circleExists = false;
+// Function to update inner HTML content
+function updateContent(html) {
+    document.getElementById('text').innerHTML = html;
+}
 
-document.getElementById('text').addEventListener('click', function(event) {
-    if (event.target && event.target.id === 'sub-heading-1') {
-        document.getElementById('text').innerHTML = '<h1 id="heading">Climate Mapping</h1><a href="#" id="link-1"><h2 id="sub-heading-1">Intro</h2></a><p id="para-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Libero enim sed faucibus turpis in eu mi. Neque laoreet suspendisse interdum consectetur libero id. Mattis molestie a iaculis at erat pellentesque adipiscing. Diam maecenas sed enim ut sem. Commodo odio aenean sed adipiscing diam donec adipiscing. Amet tellus cras adipiscing enim eu turpis egestas. Nec dui nunc mattis enim ut tellus. Magna fermentum iaculis eu non diam phasellus vestibulum. Ac orci phasellus egestas tellus rutrum tellus pellentesque eu tincidunt. Enim nunc faucibus a pellentesque sit amet. Faucibus in ornare quam viverra orci sagittis eu. Aliquet risus feugiat in ante metus dictum at tempor commodo.</p><a href="#" id="link-2"><h2 id="sub-heading-2">Mid</h2></a><p id="para-2">Aliquam sem et tortor consequat id porta nibh venenatis. Mauris cursus mattis molestie a iaculis at erat pellentesque. Non enim praesent elementum facilisis leo. Leo vel orci porta non pulvinar neque. Netus et malesuada fames ac turpis egestas integer eget. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper. Vulputate dignissim suspendisse in est. Nec ullamcorper sit amet risus. Condimentum id venenatis a condimentum vitae sapien. Neque aliquam vestibulum morbi blandit cursus. Ut tellus elementum sagittis vitae et leo. Pulvinar sapien et ligula ullamcorper malesuada. Mauris pellentesque pulvinar pellentesque habitant.</p><a href="#" id="link-3"><h2 id="sub-heading-3">End</h2></a><p id="para-3">Lobortis elementum nibh tellus molestie nunc non blandit. Arcu dui vivamus arcu felis. A diam sollicitudin tempor id. Dignissim suspendisse in est ante in. Eu tincidunt tortor aliquam nulla facilisi cras. Elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl. Consequat ac felis donec et odio. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus. Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque. Fermentum odio eu feugiat pretium nibh ipsum consequat nisl. Platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper.</p>';
-        map.flyTo([45.519292, 11.338594], 6);
+// Function to fly to location and remove layers
+function flyToAndClear(coords, zoom) {
+    map.flyTo(coords, zoom);
+    if (circleExists) {
+        map.removeLayer(circle);
+        circleExists = false;
     }
-    if (event.target && event.target.id === 'sub-heading-2') {
-        map.flyTo([35.519292, 11.338594]);
-        if (!circleExists) {
-            circle = L.circle([35.519292, 11.338594], {
-                color: 'red',
-                fillColor: '#f03',
-                fillOpacity: 0.5,
-                radius: 50000
-            }).addTo(map);
-            circleExists = true;
+    if (cannonsSet) {
+        circleLayers.forEach(layer => map.removeLayer(layer));
+        circleLayers = [];
+        cannonsSet = false;
+    }
+    if (videoExists) {
+        map.removeLayer(videoOverlay);
+        videoExists = false;
+    }
+}
+
+// Event delegation for sub-headings
+document.getElementById('text').addEventListener('click', function(event) {
+    if (event.target && event.target.id.startsWith('sub-heading')) {
+        const targetId = event.target.id;
+        if (targetId === 'sub-heading-1') {
+            flyToAndClear(defaultCoordinates, 6);
+        } else if (targetId === 'sub-heading-2') {
+            flyToAndClear([35.519292, 11.338594], 6);
+            if (!circleExists) {
+                circle = L.circle([35.519292, 11.338594], {
+                    color: 'red',
+                    fillColor: '#f03',
+                    fillOpacity: 0.5,
+                    radius: 50000
+                }).addTo(map);
+                circleExists = true;
+            }
         }
     }
 });
 
-document.getElementById('01').addEventListener('click', function() {
-    document.getElementById('text').innerHTML = '<h1>01</h1><p>Aliquam sem et tortor consequat.</p>';
-    map.flyTo([45.519292, 11.338594], 15);
-    document.getElementById('progress-bar').style.width = '0%';
-});
+// Generic function to handle simple button clicks
+function handleButtonClick(id, coords, zoom, htmlContent) {
+    document.getElementById(id).addEventListener('click', function() {
+        updateContent(htmlContent);
+        flyToAndClear(coords, zoom);
+        document.getElementById('progress-bar').style.width = '0%';
+    });
+}
 
-document.getElementById('02').addEventListener('click', function() {
-    document.getElementById('text').innerHTML = '<h1>02</h1><p>Nisi vitae suscipit tellus mauris.</p>';
-    map.flyTo([45.4709699, 11.6014322], 15);
-    document.getElementById('progress-bar').style.width = '0%';
-});
+// Setup event listeners for buttons
+handleButtonClick('00', [45.519292, 11.338594], 8, '<h1 id="heading" class="montserrat-h1">Climate Mapping</h1><a href="#" id="link-1"><h2 id="sub-heading-1" class="montserrat-h2">Intro</h2></a><p class="montserrat-p" id="para-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Libero enim sed faucibus turpis in eu mi. Neque laoreet suspendisse interdum consectetur libero id. Mattis molestie a iaculis at erat pellentesque adipiscing. Diam maecenas sed enim ut sem. Commodo odio aenean sed adipiscing diam donec adipiscing. Amet tellus cras adipiscing enim eu turpis egestas. Nec dui nunc mattis enim ut tellus. Magna fermentum iaculis eu non diam phasellus vestibulum. Ac orci phasellus egestas tellus rutrum tellus pellentesque eu tincidunt. Enim nunc faucibus a pellentesque sit amet. Faucibus in ornare quam viverra orci sagittis eu. Aliquet risus feugiat in ante metus dictum at tempor commodo.</p><a href="#" id="link-2"><h2 id="sub-heading-2" class="montserrat-h2">Mid</h2></a><p class="montserrat-p" id="para-2">Aliquam sem et tortor consequat id porta nibh venenatis. Mauris cursus mattis molestie a iaculis at erat pellentesque. Non enim praesent elementum facilisis leo. Leo vel orci porta non pulvinar neque. Netus et malesuada fames ac turpis egestas integer eget. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper. Vulputate dignissim suspendisse in est. Nec ullamcorper sit amet risus. Condimentum id venenatis a condimentum vitae sapien. Neque aliquam vestibulum morbi blandit cursus. Ut tellus elementum sagittis vitae et leo. Pulvinar sapien et ligula ullamcorper malesuada. Mauris pellentesque pulvinar pellentesque habitant.</p><a href="#" id="link-3"><h2 id="sub-heading-3" class="montserrat-h2">End</h2></a><p class="montserrat-p" id="para-3">Lobortis elementum nibh tellus molestie nunc non blandit. Arcu dui vivamus arcu felis. A diam sollicitudin tempor id. Dignissim suspendisse in est ante in. Eu tincidunt tortor aliquam nulla facilisi cras. Elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl. Consequat ac felis donec et odio. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus. Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque. Fermentum odio eu feugiat pretium nibh ipsum consequat nisl. Platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper.</p>');
+handleButtonClick('01', [45.519292, 11.338594], 15, '<h1 class="montserrat-h1">01</h1><p class="montserrat-p">Aliquam sem et tortor consequat.</p>');
+handleButtonClick('02', [45.4709699, 11.6014322], 15, '<h1 class="montserrat-h1">02</h1><p class="montserrat-p">Nisi vitae suscipit tellus mauris.</p>');
+handleButtonClick('03', [45.442492, 11.584501], 15, '<h1 class="montserrat-h1">03</h1><p class="montserrat-p">Lobortis elementum nibh tellus molestie.</p>');
+handleButtonClick('04', [46, 12], 9, '<h1 class="montserrat-h1">04</h1><p class="montserrat-p">layering</p>');
+handleButtonClick('05', [32, -130], 4, '<h1 class="montserrat-h1">05</h1><p class="montserrat-p">Video overlay</p>');
+handleButtonClick('06', [41, -1], 5, '<h1 class="montserrat-h1">06</h1><a href="#" id="link-one"><h2 id="one" class="montserrat-h2">one</h2></a><a href="#" id="link-two"><h2 id="two" class="montserrat-h2">two</h2></a><a href="#" id="link-three"><h2 id="three" class="montserrat-h2">three</h2></a>');
+handleButtonClick('07', [45.519292, 11.338594], 15, '<p class="montserrat-p">end</p>')
 
-document.getElementById('03').addEventListener('click', function() {
-    document.getElementById('text').innerHTML = '<h1>03</h1><p>Lobortis elementum nibh tellus molestie.</p>';
-    map.flyTo([45.442492, 11.584501], 15);
-    document.getElementById('progress-bar').style.width = '0%';
-});
-
+// Visualize cannons
 import { cannons } from './cannons.js';
-var cannonsSet = false;
 document.getElementById('04').addEventListener('click', function() {
-    document.getElementById('text').innerHTML = '<h1>04</h1><p>layering</p>';
-    map.flyTo([46, 12], 9);
-    document.getElementById('progress-bar').style.width = '0%';
     function createCircleMarker(feature, latlng) {
         if (feature.properties && feature.properties.cannons) {
-            return L.circle(latlng, {
+            circlee = L.circle(latlng, {
                 radius: feature.properties.cannons * 30,
                 fillColor: '#f03',
                 color: 'red',
@@ -67,60 +89,38 @@ document.getElementById('04').addEventListener('click', function() {
                 opacity: 1,
                 fillOpacity: 0.7
             });
+            circleLayers.push(circlee);
+            return circlee;
         }
     }
-
     function onEachFeature(feature, layer) {
         if (feature.properties && feature.properties.name && feature.properties.cannons) {
-            var popupContent = `${feature.properties.name} has ${feature.properties.cannons} cannons`;
+            const popupContent = `${feature.properties.name} has ${feature.properties.cannons} cannons`;
             layer.bindPopup(popupContent);
-        } else {
-            console.log("Feature with invalid properties:", feature);
         }
     }
-
     if (!cannonsSet) {
         L.geoJson(cannons, {
-            pointToLayer: function (feature, latlng) {
-                return createCircleMarker(feature, latlng);
-            },
+            pointToLayer: (feature, latlng) => createCircleMarker(feature, latlng),
             onEachFeature: onEachFeature
         }).addTo(map);
+        cannonsSet = true;
     }
-    cannonsSet = true;
 });
 
-var videoUrls = 'https://user.eumetsat.int/s3/eup-strapi-media/vid_il_19_07_07_3_76b9938111.mp4';
-var latLngBounds = L.latLngBounds([[32, -130], [13, -100]]);
-var videoOverlay;
+// Video overlay handling
+const videoUrl = 'https://user.eumetsat.int/s3/eup-strapi-media/vid_il_19_07_07_3_76b9938111.mp4';
+const latLngBounds = L.latLngBounds([[32, -130], [13, -100]]);
+let videoOverlay;
 document.getElementById('05').addEventListener('click', function() {
-    map.flyTo([32, -130], 4);
-    document.getElementById('progress-bar').style.width = '0%';
-    videoOverlay = L.videoOverlay(videoUrls, latLngBounds, {
-        opacity: 0.6,
-        interactive: true,
-        autoplay: true,
-        muted: true,
-        playsInline: true
-    }).addTo(map);
-});
-
-// var storm1 = '';
-// var points1 = L.latLngBounds([[41.315, -1.911], [51.907, 31.289]]);
-// var storm2 = 'media/Unsaved_session_2023-07-19_16_00-2023-07-20_05_00 (1).mp4';
-// var points2 = L.latLngBounds([[41.300, -8.155], [51.895, 25.046]]);
-// var storm3 = 'media/Unsaved_session_2023-07-24_00_00-2023-07-25_15_45.mp4';
-// var points3 = L.latLngBounds([[41.104, -6.695], [51.734, 26.506]]);
-// var vid1, vid2, vid3;
-document.getElementById('06').addEventListener('click', function() {
-    document.getElementById('text').innerHTML = '<h1>06</h1><a href="#" id="link-one"><h2 id="one">one</h2></a><a href="#" id="link-one"><h2 id="two">two</h2></a><a href="#" id="link-one"><h2 id="three">three</h2></a>';
-    map.flyTo([41, -1], 5);
-    document.getElementById('progress-bar').style.width = '0%';
-    vid1 = L.videoOverlay(storm1, points1, {
-        opacity: 0.6,
-        interactive: true,
-        autoplay: true,
-        muted: true,
-        playsInline: true
-    }).addTo(map);
+    if (!videoExists) {
+        videoOverlay = L.videoOverlay(videoUrl, latLngBounds, {
+            opacity: 0.6,
+            interactive: true,
+            autoplay: true,
+            muted: true,
+            playsInline: true
+        }).addTo(map);
+        videoExists = true;
+    }
 });
